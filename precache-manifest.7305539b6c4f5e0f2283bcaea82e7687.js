54709311780350757752a3107e6a522f079a50db
seasons986 <annalin12345@gmail.com>
2019-12-28 13:07:20 -0800

    Build only in master

---
+++ precache-manifest.7305539b6c4f5e0f2283bcaea82e7687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be20aa5345c646d923c4c23a5861b81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b38895b06356ca3c036",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0f747272ab797a52cc8c",
+    "url": "/static/js/2.f79f8c38.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f79f8c38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b38895b06356ca3c036",
+    "url": "/static/js/main.9c8ec3fb.chunk.js"
+  },
+  {
+    "revision": "e9ab73750fca670ba889",
+    "url": "/static/js/runtime-main.c59ba31e.js"
+  },
+  {
+    "revision": "930c98149f737c09177c795b79bd3307",
+    "url": "/static/media/logo.930c9814.svg"
+  }
+]);
