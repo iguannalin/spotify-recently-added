713ae69a355103c0c9c1bd02d5fcb83be8955ab3
seasons986 <annalin12345@gmail.com>
2019-12-29 23:29:28 -0800

    Updates

---
+++ precache-manifest.6afa7d8c2fb747cf2566f59e706b5751.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8a49af74878d4f2b83a33c09e9cd6d",
+    "url": "/spotify-recently-added/index.html"
+  },
+  {
+    "revision": "8afcdacaf39f35244028",
+    "url": "/spotify-recently-added/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8d6bcb9822a27634e5bc",
+    "url": "/spotify-recently-added/static/js/2.50ef68ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spotify-recently-added/static/js/2.50ef68ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8afcdacaf39f35244028",
+    "url": "/spotify-recently-added/static/js/main.a2569e45.chunk.js"
+  },
+  {
+    "revision": "9c3d74df65d04d7a5ecc",
+    "url": "/spotify-recently-added/static/js/runtime-main.ede6e71b.js"
+  },
+  {
+    "revision": "930c98149f737c09177c795b79bd3307",
+    "url": "/spotify-recently-added/static/media/logo.930c9814.svg"
+  }
+]);
