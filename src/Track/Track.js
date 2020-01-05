e719003dc40d44e80cb104679c966446cd7c9614
seasons986 <annalin12345@gmail.com>
2020-01-05 02:50:11 -0800

    New layout for tracks with album cover, and appropriate alt tags

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
+import './Track.scss';
 
 class Track extends Component {
     render() {
         return (
             <div className="Track">
-                <div className="track-title">
-                    <a href={this.props.track.link}>{this.props.track.name}</a>
+                <div className="track-image-small">
+                    <img alt={'Album cover of ' + this.props.track.name + ' by ' + this.props.track.artists[0].name} src={this.props.track.albumArt[1].url}/>
                 </div>
-                <div className="track-artist">
-                    {
-                        this.props.track.artists.map((artist, index) => {
-                            return (<a href={artist.link} key={index}>{artist.name}</a>);
-                        })
-                    }
+                <div className="track-details">
+                    <div className="track-title">
+                        <a href={this.props.track.link}>{this.props.track.name}</a>
+                    </div>
+                    <div className="track-artist">
+                        {
+                            this.props.track.artists.map((artist, index) => {
+                                return (<a href={artist.link} key={index}>{artist.name}</a>);
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         );
